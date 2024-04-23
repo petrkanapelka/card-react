19974534ba5a351e553d1605f0150a2cf25bd227
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-24 00:35:24 +0300

    feat: create box wrapper

---
+++ card-react/src/components/Box.styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 300px;
+    height: 350px;
+    background: #ffffff;
+    border-radius: 15px;
+    box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.1);
+    padding: 10px 10px;
+`;
+
+export default Box;
