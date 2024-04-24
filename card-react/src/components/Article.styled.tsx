f6a3615270db92b5bfb3d64a932e90176cf33f3e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-24 21:05:24 +0300

    feat: add ArticleContentType and props

--- card-react/src/components/Article.styled.tsx
+++ card-react/src/components/Article.styled.tsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
-const articleContent = 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.';
+export const articleContent = 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.';
 
 const Article = styled.p`
     font-size: 1.2rem;
     font-weight: 500;
 `
-
-function ArticleStyled() {
+type ArticleContentType = {
+    content: string;
+}
+function ArticleStyled(props: ArticleContentType) {
     return (
-        <Article>{articleContent}</Article>
+        <Article>{props.content}</Article>
     )
 }
 
